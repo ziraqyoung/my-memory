44190f73ab0c88487cfa7f5d71e064fa46e8a1b2
ziraq young <ziraqyoung@outlook.com>
2021-08-08 12:57:29 +0300

    Setup RecyclerView for the Game Board with ImageButtons
    - Register the main components of the RV (adapter, layoutManager)
    - Define custom MemoryBoardAdapter
    - Create memory_card.xml layout file for individual item in the RV
    - Set individual items in the Rv to have dynamic width, height and a
      constant MARGIN_SIZE
    - Get hold on the button size upon clicked via setOnClickListener
      function

--- app/src/main/java/com/github/ziraqyoung/mymemory/MainActivity.kt
+++ app/src/main/java/com/github/ziraqyoung/mymemory/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.ziraqyoung.mymemory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,12 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        // 3 components of any Android RecyclerView (adapter, layoutManaget)
+        // setHasFixedSize is an optimization when layout do not change
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
 
     }
 }
