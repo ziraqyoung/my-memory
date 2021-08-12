20bddd054504efc9a38745c2904f9dfcf68aada9
ziraq young <ziraqyoung@outlook.com>
2021-08-12 22:03:17 +0300

    Implementing the Game logic
    - Add ability to flip over a card when appropriately
    - Define gray color for already flipped card
    - Add Error Handling by showing Snackbar, when the game is won or
      invalid move
    - Add clRoot id to ConstraintLayout, to mount Snackbar

--- app/src/main/java/com/github/ziraqyoung/mymemory/MainActivity.kt
+++ app/src/main/java/com/github/ziraqyoung/mymemory/MainActivity.kt
@@ -4,45 +4,72 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.ziraqyoung.mymemory.models.BoardSize
 import com.github.ziraqyoung.mymemory.models.MemoryCard
 import com.github.ziraqyoung.mymemory.models.MemoryGame
 import com.github.ziraqyoung.mymemory.utils.DEFAULT_ICONS
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
 
+    private lateinit var adapter: MemoryBoardAdapter
+    private lateinit var memoryGame: MemoryGame
+
+    private lateinit var clRoot: ConstraintLayout
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
 
-    private val boardSize :BoardSize = BoardSize.EASY
+    private val boardSize :BoardSize = BoardSize.MEDIUM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clRoot = findViewById(R.id.clRoot)
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
 
-        val memoryGame = MemoryGame(boardSize)
+         memoryGame = MemoryGame(boardSize)
 
         // 3 components of any Android RecyclerView (adapter, layoutManager)
         // setHasFixedSize is an optimization when layout do not change
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
             override fun onCardClicked(position: Int) {
                 Log.i(TAG, "Card clicked $position")
+                updateGameWithFlip(position)
             }
-
         })
+        rvBoard.adapter = adapter
         rvBoard.setHasFixedSize(true)
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
 
     }
+
+    private fun updateGameWithFlip(position: Int) {
+        // Error checking
+        if(memoryGame.haveWonGame()) {
+            Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+         if(memoryGame.isCardFaceUp(position)) {
+             Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_LONG).show()
+             return
+         }
+
+        // Actually flip over the card at this position
+        if (memoryGame.flipCard(position)) {
+            Log.i(TAG, "Found a match! Num pairs found ${memoryGame.numPairsFound}")
+        }
+        adapter.notifyDataSetChanged()
+    }
+
 }
--- app/src/main/java/com/github/ziraqyoung/mymemory/models/MemoryGame.kt
+++ app/src/main/java/com/github/ziraqyoung/mymemory/models/MemoryGame.kt
@@ -3,12 +3,63 @@ package com.github.ziraqyoung.mymemory.models
 import com.github.ziraqyoung.mymemory.utils.DEFAULT_ICONS
 
 class MemoryGame(private val boardSize: BoardSize) {
+
     val cards: List<MemoryCard>
-    val numPairs = 0
+    var numPairsFound = 0
+
+    private var indexOfSingleSelectedCard: Int? = null
 
     init {
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
         val randomizedImages = (chosenImages + chosenImages).shuffled()
         cards = randomizedImages.map { MemoryCard(it ) }
+    }
+
+    fun flipCard(position: Int) : Boolean {
+        val card = cards[position]
+        // Three cases
+        // 0 cards flipped over => restore cards + flip over the selected card
+        // 1 card previously flipped over => flip over selected card + check if the images matches
+        // 2 cards previously flipped over => restore cards + flip over the selected card
+        var foundMatch = false
+        if (indexOfSingleSelectedCard == null) {
+            // 0 or 2 previously selected cards
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        } else {
+            // exactly one card previously flipped over
+             foundMatch = checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
+        card.isFaceUp = !card.isFaceUp
+
+        return foundMatch
+    }
+
+    private fun checkForMatch(position1:  Int, position2: Int): Boolean {
+        if(cards[position1].identifier != cards[position2].identifier) {
+            return false
+        }
+        cards[position1].isMatched = true
+        cards[position2].isMatched = true
+        numPairsFound++
+
+        return true
+    }
+
+    private fun restoreCards() {
+        for(card in cards) {
+            if(!card.isMatched) {
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    fun haveWonGame(): Boolean {
+        return numPairsFound == boardSize.getNumPairs()
+    }
+
+    fun isCardFaceUp(position: Int): Boolean {
+        return cards[position].isFaceUp
     }
 }
